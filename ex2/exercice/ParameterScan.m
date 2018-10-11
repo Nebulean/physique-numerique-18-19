c55725358dfbe809898e7b8f6e6f15fef6db900c
Delphine Martres <dmartres@gmail.com>
2018-10-11 14:07:54 +0200

    Plop

--- ex2/exercice/ParameterScan.m
+++ ex2/exercice/ParameterScan.m
@@ -1,7 +1,7 @@
 % Ce script Matlab automatise la production de resultats
 % lorsqu'on doit faire une serie de simulations en
 % variant un des parametres d'entree.
-% 
+%
 % Il utilise les arguments du programme (voir ConfigFile.h)
 % pour remplacer la valeur d'un parametre du fichier d'input
 % par la valeur scannee.
@@ -17,7 +17,7 @@ input = 'configuration.in'; % Nom du fichier d'entree de base
 nsimul = 20; % Nombre de simulations a faire
 
 nsteps = round(logspace(2,4,nsimul)); % Nombre d'iterations entier de 10^2 a 10^4
-tfin = 0; % TODO: Remplacer la valeur de tfin
+tfin = 1.09321e-7;
 dt = tfin ./ nsteps;
 
 paramstr = 'nsteps'; % Nom du parametre a scanner
@@ -37,6 +37,16 @@ end
 
 %% Analyse %%
 %%%%%%%%%%%%%
+tfin   = 1.09321e-7;
+q      = 1.6022e-19;
+m      = 1.6726e-27;
+B0     = 3;
+E      = 0;
+Kappa  = 0;
+x0     = -vy0*m/(q*B0);
+y0     = 0;
+vx0    = 0;
+vy0    = 4e5;
 
 error = zeros(1,nsimul);
 for i = 1:nsimul % Parcours des resultats de toutes les simulations
@@ -44,8 +54,8 @@ for i = 1:nsimul % Parcours des resultats de toutes les simulations
     t = data(:,1);
     x = data(:,2);
     y = data(:,3);
-    x_th = sin(t); % TODO: Entrer la vraie solution analytique en fonction du temps
-    y_th = cos(t); % TODO: Entrer la vraie solution analytique en fonction du temps
+    x_th = sin(q*B0*t/m); % TODO: Entrer la vraie solution analytique en fonction du temps
+    y_th = -v0*cos(q*B0*t/m); % TODO: Entrer la vraie solution analytique en fonction du temps
     error(i) = max(sqrt((x-x_th).^2+(y-y_th).^2));
 end
 
@@ -54,6 +64,3 @@ loglog(dt, error, 'k+')
 xlabel('\Delta t')
 ylabel('Maximum de l''erreur sur la position')
 grid on
-
-
-
