8031ed0bf07942182c8169e6555cb2265748059e
Damien Korber <damien@korber.email>
2018-10-14 22:45:17 +0200

    Updated report, add graphs.

---
+++ ex2/exercice/application1_iii.m
@@ -0,0 +1,19 @@
+% on fait la simulation
+cmd = './Exercice2 configuration.in nsteps=500 E=6e4 B0=3 vx0=-2e4 vy0=4e5 tfin=1.09321e-7 schema=EulerCromer output=app1_iii.out';
+disp(cmd);
+system(cmd);
+
+% on traite les données
+d = load('app1_iii.out');
+x = d(:,2);
+y = d(:,3);
+
+% On plot
+fig = figure;
+p = plot(x,y);
+
+set(p, 'LineWidth',1.5);
+set(gca, 'fontsize',16);
+
+xlabel('x [m]');
+ylabel('y [m]');
