0d23da394b226734edc3991db8af788403395f12
Delphine Martres <dmartres@gmail.com>
2018-10-26 11:46:50 +0200

    gerard

---
+++ ex3/exercice/gerard.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#a
+./Exercice3 configuration.in kappa=0 d=0 theta0=10e-6 thetadot0=0 tfin=20 output=a.out
+
+#b
+./Exercice3 configuration.in kappa=0. d=0. thetadot0=0 tFin=20
