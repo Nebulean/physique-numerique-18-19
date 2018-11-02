c6d7ff8cbba8728ccd5e4ea8a46039dc540c9a8e
Damien Korber <damien@korber.email>
2018-11-02 11:37:49 +0100

    Updated graphs

--- ex3/exercice/d_thm.m
+++ ex3/exercice/d_thm.m
@@ -7,12 +7,40 @@ emec = d(:,4);
 pnc = d(:,5);
 emecdot = d(:,6);
 
-f = figure;
+f1 = figure;
 hold on;
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+set(groot, 'defaultTextInterpreter', 'latex');
+set(groot, 'defaultAxesFontSize', 18);
+set(gca, 'fontsize', 22);
+
 plot(t, emecdot-pnc);
 
+ymin=-2e-16;
+ymax=2e-16;
+ylim([ymin, ymax]);
+
 xlabel("t [s]");
-ylabel("$\frac{dP}{dt} - P_{nc}$ [W]")
+ylabel("$\frac{d(E_m)}{dt} - P_{nc}$ [W]")
 
 grid on;
 hold off;
+
+f2 = figure;
+hold on;
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+set(groot, 'defaultTextInterpreter', 'latex');
+set(groot, 'defaultAxesFontSize', 18);
+set(gca, 'fontsize', 22);
+
+plot(t,theta);
+
+xlabel("t [s]");
+ylabel("$\theta$ [rad]")
+
+hold off;
+
+saveas(f1, 'graphs/d_thm','epsc');
+saveas(f2, 'graphs/d_theta','epsc');
--- ex3/exercice/gerard.sh
+++ ex3/exercice/gerard.sh
@@ -11,5 +11,5 @@ echo "Simulation de l'exercice B."
 ./Exercice3 configuration.in kappa=0. d=0. thetadot0=0 tFin=20 output=b.out
 
 #d
-./Exercice3 configuration.in theta0=0. thetadot0=1e-2 Omega=19.809088823 d=0.005 kappa=0.05 tFin=100 output=d_thm.out
+./Exercice3 configuration.in theta0=0. thetadot0=1e-2 Omega=19.809 d=0.005 kappa=0.05 tFin=100 output=d_thm.out
 #19.809088823
