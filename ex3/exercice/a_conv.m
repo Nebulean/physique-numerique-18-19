fd809f7e555ed6e1ea0ae7a8fed211f13e804171
Damien Korber <damien@korber.email>
2018-11-09 12:24:01 +0100

    Fixed conv graph for a, maybe a fix for theorem in d, minor change in report

--- ex3/exercice/a_conv.m
+++ ex3/exercice/a_conv.m
@@ -9,18 +9,19 @@ repertoire = './'; % Chemin d'acces au code compile (NB: enlever le ./ sous Wind
 executable = 'Exercice3'; % Nom de l'executable (NB: ajouter .exe sous Windows)
 input = 'configuration.in'; % Nom du fichier d'entree de base
 
-nsimul = 100; % Nombre de simulations a faire
+nsimul = 20; % Nombre de simulations a faire
 nsteps = logspace(3,5,nsimul); % Nombre d'iterations entier de 10^2 a 10^4
 tfin = 20;
 dt = tfin ./ nsteps;
 paramstr = 'dt';
+param=dt;
 
 % SIMULATIONS
 output = cell(1, nsimul); % Tableau de cellules contenant le nom des fichiers de sortie
 for i = 1:nsimul
     output{i} = [paramstr, '=', num2str(dt(i)), '.out'];
     % Execution du programme en lui envoyant la valeur a scanner en argument
-    cmd = sprintf('%s%s %s %s=%.15g output=%s kappa=0. d=0. theta0=1e-6 thetadot0=0. tfin=20', repertoire, executable, input, paramstr, dt(i), output{i});
+    cmd = sprintf('%s%s %s %s=%.15g output=%s kappa=0. d=0. theta0=1e-6 thetadot0=0. tfin=20 g=9.81', repertoire, executable, input, paramstr, dt(i), output{i});
     disp(cmd)
     system(cmd); % À commenter quand les simulations sont faites.
 end
@@ -32,25 +33,27 @@ A = 1e-6;
 g = 9.81;
 L = 0.1;
 omega0 = sqrt(g/L);
-theta_th = A*cos(omega0*20);
+%theta_th = A*cos(omega0*20);
 
 % On calcul le tout
-%error = ones(1,nsimul);
-thetafin = zeros(0,nsimul);
+error = ones(1,nsimul);
+thetafin = zeros(1,nsimul);
 for i=1:nsimul
     output = sprintf('dt=%s.out', num2str(dt(i)));
     d = load(output);
-
-    theta = d(end,2);
-    thetafin(i) = theta;
-    error(i) = abs(theta - theta_th)
+    
+    timefin = d(end,1);
+    thetafin = d(end,2);
+    thetath = A*cos(omega0*timefin);
+    
+    error(i) = abs(thetafin - thetath);
 end
 
-
+%% Graph de la figure
 f = figure
 hold on;
 
-p = plot(dt, error, '-','LineWidth',1.2);
+p = plot(dt, error, 'x','LineWidth',1.2);
 
 xlabel('$\Delta t$ [s]');
 ylabel('Error on the final angle $\theta$')
--- ex3/exercice/d_thm.m
+++ ex3/exercice/d_thm.m
@@ -1,5 +1,6 @@
 d = load("d_thm.out");
 
+% t theta thetadot emec pnc emecdot
 t = d(:,1);
 theta = d(:,2);
 thetadot = d(:,3);
@@ -15,7 +16,12 @@ set(groot, 'defaultTextInterpreter', 'latex');
 set(groot, 'defaultAxesFontSize', 18);
 set(gca, 'fontsize', 22);
 
-plot(t, emecdot-pnc);
+
+emecdot2 = diff(emec)./diff(t);
+pnc(end) = [];
+t(end) = [];
+
+plot(t, emecdot2-pnc);
 
 ymin=-2e-16;
 ymax=2e-16;
@@ -39,6 +45,25 @@ plot(t,theta);
 
 xlabel("t [s]");
 ylabel("$\theta$ [rad]")
+grid on;
+
+hold off;
+
+%% graph emecdot
+f3 = figure;
+hold on;
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+set(groot, 'defaultTextInterpreter', 'latex');
+set(groot, 'defaultAxesFontSize', 18);
+set(gca, 'fontsize', 22);
+
+%emecdot2 = diff(emec)./diff(t);
+%t(end) = [];
+plot(t,emecdot);
+
+xlabel("t [s]");
+ylabel("derivative of mecanical energy [J]")
 grid on;
 
 hold off;
