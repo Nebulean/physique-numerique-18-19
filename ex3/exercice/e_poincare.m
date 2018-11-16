cbdc32d8d5510c7ffc248ede2c20feef299d914e
Damien Korber <damien@korber.email>
2018-11-16 10:28:41 +0100

    Updated e_poincaré graph

--- ex3/exercice/e_poincare.m
+++ ex3/exercice/e_poincare.m
@@ -6,30 +6,31 @@ omega0 = sqrt(g/L);
 
 Omega = omega0;
 
-n = 100; % On choisit le nombre de de pas de temps que l'on veut avoir dans chaque période.
+n = 1000; % On choisit le nombre de de pas de temps que l'on veut avoir dans chaque période.
 
 tfin = 10000*2*pi/Omega; % Le cas où i = 10000 est celui de base, mais j'ai pris un peu plus.
 
 dt = 2*pi/(n*Omega);
 
 %% CHOIX DE DIFFERENTES CONDITIONS INITIALES
-% Big brother is watching you. -> A GARDER: CAS NON-CHAOTIQUE (?)
-theta0 = 0.;
-thetadot0 = 1e-2;
-figname = "bigbrother";
+% MK8 % -> A GARDER PETIT MOUVEMENTS
+% theta0 = 0.;
+% thetadot0 = 1e-2;
+% figname = "MK8";
 
 % Une donut mince et compacte
-% theta0 = pi/2;
-% thetadot0 = 1e-2;
+% theta0 = 3*pi/4;
+% thetadot0 = 0;
+% filename = "lemon";
 
 % Un donut moins mince avec des motifs plus joli.
 % theta0 = pi/3;
 % thetadot0 = pi;
 
 % Motifs complétement chaotique. -> A GARDER: CAS CHAOTIQUE
-% theta0 = 3.12;
-% thetadot0 = 1e-2;
-% figname = "chaos";
+theta0 = 3.12;
+thetadot0 = 1e-2;
+figname = "chaos";
 
 % Big fat donut très joli. -> A GARDER: CAS NON-CHAOTIQUE
 % theta0 = 1e-6;
@@ -58,10 +59,11 @@ thetadot = d(:,3);
 fig=figure;
 hold on;
 
-plot(thetadot, theta, '.');
+plot(theta, thetadot, '.');
 
-xlabel("$\dot{\theta}$ [rad/s]");
-ylabel("$\theta$ [rad]")
+xlabel("$\theta$ [rad]");
+ylabel("$\dot{\theta}$ [rad/s]");
+
 grid on;
 
 hold off;
