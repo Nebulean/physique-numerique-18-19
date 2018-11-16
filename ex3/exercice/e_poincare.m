8d430c0564f43f1449fcc45debaf9fd2defad280
Delphine Martres <dmartres@gmail.com>
2018-11-16 09:35:40 +0100

    Improved precision of Omega and dt

--- ex3/exercice/e_poincare.m
+++ ex3/exercice/e_poincare.m
@@ -6,17 +6,17 @@ omega0 = sqrt(g/L);
 
 Omega = omega0;
 
-n = 500; % On choisit le nombre de de pas de temps que l'on veut avoir dans chaque période.
+n = 100; % On choisit le nombre de de pas de temps que l'on veut avoir dans chaque période.
 
-tfin = 30000*2*pi/Omega; % Le cas où i = 10000 est celui de base, mais j'ai pris un peu plus.
+tfin = 10000*2*pi/Omega; % Le cas où i = 10000 est celui de base, mais j'ai pris un peu plus.
 
 dt = 2*pi/(n*Omega);
 
 %% CHOIX DE DIFFERENTES CONDITIONS INITIALES
 % Big brother is watching you. -> A GARDER: CAS NON-CHAOTIQUE (?)
-% theta0 = 0.;
-% thetadot0 = 1e-2;
-% figname = "bigbrother";
+theta0 = 0.;
+thetadot0 = 1e-2;
+figname = "bigbrother";
 
 % Une donut mince et compacte
 % theta0 = pi/2;
@@ -27,7 +27,7 @@ dt = 2*pi/(n*Omega);
 % thetadot0 = pi;
 
 % Motifs complétement chaotique. -> A GARDER: CAS CHAOTIQUE
-% theta0 = pi;
+% theta0 = 3.12;
 % thetadot0 = 1e-2;
 % figname = "chaos";
 
@@ -37,13 +37,14 @@ dt = 2*pi/(n*Omega);
 % figname = "fatdonut";
 
 % Petits mouvements -> A GARDER: CAS NON-CHAOTIQUE
-theta0 = 1e-6;
-thetadot0 = 0.;
-figname = "littlemoves";
+% theta0 = 1e-6;
+% thetadot0 = 0.;
+% figname = "littlemoves";
 
 %% On lance la simulation
-cmd = sprintf("./Exercice3 configuration.in Omega=%s d=0.04 kappa=0. theta0=%s thetadot0=%s dt=%s tFin=%s sampling=%s output=e_poincare.out", num2str(Omega), num2str(theta0), num2str(thetadot0), num2str(dt), num2str(tfin), num2str(n))
+cmd = sprintf("./Exercice3 configuration.in Omega=%0.15f d=0.04 kappa=0. theta0=%f thetadot0=%f dt=%0.15f tFin=%f sampling=%d output=e_poincare.out", Omega, theta0, thetadot0, dt, tfin, n)
 system(cmd);
+disp(cmd);
 
 
 
