1dd24be0c2b03d4e324a1a34f69aa4446564a90a
Delphine Martres <dmartres@gmail.com>
2018-11-17 22:30:46 +0100

    Wrote end of e

--- ex3/exercice/e_sens.m
+++ ex3/exercice/e_sens.m
@@ -20,9 +20,9 @@ thetadot0 = 0.;
 figname = "chaos";
 
 % NOT CHAOS, BORING
-% theta01 = 1e-6;
-% thetadot0 = 1.;
-% figname = "notchaos";
+theta01 = 0.;
+thetadot0 = 1.;
+figname = "notchaos";
 
 % deuxième graph
 theta02 = theta01+1e-8;
@@ -65,20 +65,23 @@ plot(theta1, thetadot1, '.', theta2, thetadot2, '.');
 set(gca, 'fontsize', 22);
 xlabel("$\theta$ [rad]");
 ylabel("$\dot{\theta}$ [rad/s]");
-legend("$\theta_0=\pi$","$\theta_0=\pi+1 \cdot 10^{-8}$");
+legend("$\theta_0=0$","$\theta_0=10^{-8}$");
 grid on;
 hold off;
 
 f2=figure;
 hold on;
 set(gca, 'YScale', 'log');
-plot(t,d);
+plot(t,d, 'x');
 set(gca, 'fontsize', 22);
+xlabel("$t [s]$");
+ylabel("difference $d$");
 grid on
 hold off;
 
 wheretosave = sprintf("graphs/e_sens_%s", figname);
-saveas(f1, wheretosave, 'epsc');
+% saveas(f1, wheretosave, 'epsc');
+print(f1, wheretosave,'-dpng','-r600');
 
 wheretosave = sprintf("graphs/e_lyap_%s", figname);
 saveas(f2, wheretosave, 'epsc');
