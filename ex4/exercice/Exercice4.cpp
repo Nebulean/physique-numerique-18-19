f4bb8bd65121fe1db74b5aae1dbfb78ff7db9425
Damien Korber <damien@korber.email>
2018-11-25 14:28:15 +0100

    Added multiple functions.

--- ex4/exercice/Exercice4.cpp
+++ ex4/exercice/Exercice4.cpp
@@ -19,10 +19,8 @@ private:
   int last;
   ofstream *outputFile;
   valarray<double> p;   // vector containing all the informations about position and speed.
-                        // Convention: (r1x, r1y, ... , rNx, rNy, v1x, v1y, ..., vNx, vNy)
-                        // If there is Apollo 13, it is always the LAST body of the list.
-                        // Earth is ALWAYS the first body.
-  valarray<double> m;   // vector containing all the masses
+                        // Convention: (x1, y1, vx1, vy1, ..., x3, y3, vx3, vy3)
+  double m1, m2, m3;    // mass of bodies
   bool atm;             // false if no atmosphere, true is atmosphere.
   bool dtad;            // indicates whether run() should use the adaptative dt or not
 
@@ -44,74 +42,115 @@ private:
     }
   }
 
-  double grav(double m1, double m2, double r){
-    return -G * m1 * m2 / pow(r,2);
+  // returns the position of the body.
+  valarray<double> getPos(size_t body){
+    valarray<double> r(2);
+    r[0] = p[4*body];
+    r[1] = p[4*(body + 1)];
+    return r;
+  }
+
+  // returns the velocity of the body.
+  valarray<double> getVel(size_t body){
+    valarray<double> v(2);
+    v[0] = p[4*(body + 2)];
+    v[1] = p[4*(body + 3)];
+    return v;
+  }
+
+  // returns the euclidian norm of the array. (as if it were a vector)
+  double norm(valarray<double> const& v){
+    double res(0);
+    for (auto const& el : v) {
+      res += pow(el,2);
+    }
+    return sqrt(res);
+  }
+
+  // returns the gravitational effect of mass 2 on mass 1.
+  valarray<double> grav(double mass1, double mass2, valarray<double> const& r){
+    valarray<double> g(2);
+    g = -G * mass1 * mass2 * (1./pow(norm(r),3)) * r;
+    return g;
+  }
+
+  // returns the air density on earth.
+  double rho(valarray<double> const& r){
+    double n(norm(r));
+    return rho0 * exp(-(n - Rt)/lambda);
+  }
+
+  valarray<double> drag(valarray<double> const& r, valarray<double> const& v){
+    valarray<double> f(2);
+    f = -0.5 * rho(r) * S * Cx * norm(v) * v;
+    return f;
   }
 
   double a(size_t body){ // "body" is the mass where the force is applied. Body starts at 0 !!!
     //TODO : CE N'EST PAS UNE VERSION CORRECTE, C'EST JUSTE UNE PERMIERE ETAPE DE SA REALISATION
-    double result(0);
-    double dist(0);
-    double grav_effect(0);
-    double atm_effect(0);
-
-
-    // the positions are defined, it simplifies the notations.
-    // the corps with index 'body' stays the same.
-    double x1(p[2*body]);
-    double x2(0);
-    double y1(p[2*body + 1]);
-    double y2(0);
-    double vx1(p[p.size()/2. + 2*body]);
-    double vx2(0);
-    double vy1(p[p.size()/2. + 2*body + 1]);
-    double vy2(0);
-
-    // ====================== GRAVITATIONAL EFFECT ============================
-    // This for loop will compute the gravitational effect on the body.
-    for (size_t i = 0; i < m.size(); i++) {
-      // check that the body is not affected by itself
-      if (i == body)
-        i++;
-      if (i >= m.size())
-        return grav_effect;
-
-      // computation of the distance between both objects
-      x2 = p[2*i]; y2 = p[2*i + 1];
-      dist = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
-
-      // Computation of the effect of gravity
-      grav_effect += grav(m[body], m[i], dist);
-    }
-    // TODO: On a calculé la norme, il faut encore le projeter.
-
-    // ======================== ATMOSPHERE EFFECT ==============================
-    // we add the effect of atmosphere on Apollo 13, which is always the LAST body in p.
-    // If this effect does not apply, or there is no Apollo 13,
-    if (atm && (body == m.size() - 1)) {
-      // computation of the distance between earth and Apollo 13.
-      // first, we compute the distance between both bodies
-      x2 = p[0];
-      y2 = p[1];
-      dist = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
-
-      // then, the difference of speed
-      vx2 = p[p.size()/2.];
-      vy2 = p[p.size()/2. + 1];
-      double diff_speed = sqrt(pow(vx2 - vx1,2) + pow(vy2 - vy1,2));
-
-      // then, rho
-      double rho( rho0 * exp( - (dist - Rt)/lambda) );
-
-      // then, the effect of earth's atmosphere
-      atm_effect = -0.5 * rho * S * Cx * diff_speed;
-    }
-    // TODO: On a calculé la norme, il faut encore le projeter.
+    // double result(0);
+    // double dist(0);
+    // double grav_effect(0);
+    // double atm_effect(0);
+    //
+    //
+    // // the positions are defined, it simplifies the notations.
+    // // the corps with index 'body' stays the same.
+    // double x1(p[2*body]);
+    // double x2(0);
+    // double y1(p[2*body + 1]);
+    // double y2(0);
+    // double vx1(p[p.size()/2. + 2*body]);
+    // double vx2(0);
+    // double vy1(p[p.size()/2. + 2*body + 1]);
+    // double vy2(0);
+    //
+    // // ====================== GRAVITATIONAL EFFECT ============================
+    // // This for loop will compute the gravitational effect on the body.
+    // for (size_t i = 0; i < m.size(); i++) {
+    //   // check that the body is not affected by itself
+    //   if (i == body)
+    //     i++;
+    //   if (i >= m.size())
+    //     return grav_effect;
+    //
+    //   // computation of the distance between both objects
+    //   x2 = p[2*i]; y2 = p[2*i + 1];
+    //   dist = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+    //
+    //   // Computation of the effect of gravity
+    //   grav_effect += grav(m[body], m[i], dist);
+    // }
+    // // TODO: On a calculé la norme, il faut encore le projeter.
+    //
+    // // ======================== ATMOSPHERE EFFECT ==============================
+    // // we add the effect of atmosphere on Apollo 13, which is always the LAST body in p.
+    // // If this effect does not apply, or there is no Apollo 13,
+    // if (atm && (body == m.size() - 1)) {
+    //   // computation of the distance between earth and Apollo 13.
+    //   // first, we compute the distance between both bodies
+    //   x2 = p[0];
+    //   y2 = p[1];
+    //   dist = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+    //
+    //   // then, the difference of speed
+    //   vx2 = p[p.size()/2.];
+    //   vy2 = p[p.size()/2. + 1];
+    //   double diff_speed = sqrt(pow(vx2 - vx1,2) + pow(vy2 - vy1,2));
+    //
+    //   // then, rho
+    //   double rho( rho0 * exp( - (dist - Rt)/lambda) );
+    //
+    //   // then, the effect of earth's atmosphere
+    //   atm_effect = -0.5 * rho * S * Cx * diff_speed;
+    // }
+    // // TODO: On a calculé la norme, il faut encore le projeter.
   }
 
 
 
   valarray<double> step(valarray<double> const& v, double time_step){
+    //TODO: Cette version est ancienne, il faut la mettre à jour. (En attente de l'accélération)
     // RK4
     // some initialisations
     size_t mid = v.size()/2.;
@@ -190,13 +229,12 @@ private:
       return p2;
     } else {
       dt*=0.99*pow(epsilon/d, 1./5.);
-      dtadapt();
+      dtadapt(); //TODO: y'a pas de retour ?
     }
   }
 
 public:
-  Engine(int argc, char* argv[])
-  {
+  Engine(int argc, char* argv[]) {
     string inputPath("configuration.in"); // Fichier d'input par defaut
     if(argc>1) // Fichier d'input specifie par l'utilisateur ("./Exercice3 config_perso.in")
       inputPath = argv[1];
@@ -205,13 +243,15 @@ public:
 
     for(int i(2); i<argc; ++i) // Input complementaires ("./Exercice3 config_perso.in input_scan=[valeur]")
       configFile.process(argv[i]);
-      //TODO: Il faut que l'on implémente nos propres variables.
+
     tFin        = configFile.get<double>("tFin");
     dt          = configFile.get<double>("dt");
     atm         = configFile.get<bool>("atm");
     epsilon     = configFile.get<double>("epsilon");
     dtad        = configFile.get<double>("dtad");
-    // m           = configFile.get<valarray<double> >("m"); //TODO: Comment faire pour avoir une liste d'éléments ?
+    m1          = configFile.get<double>("m1");
+    m2          = configFile.get<double>("m2");
+    m3          = configFile.get<double>("m3");
     // d        = configFile.get<double>("d");
     // Omega    = configFile.get<double>("Omega");
     // kappa    = configFile.get<double>("kappa");
@@ -232,8 +272,7 @@ public:
     delete outputFile;
   }
 
-  void run()
-  {
+  void run() {
     t = 0.;
     last = 0;
     printOut(true);
