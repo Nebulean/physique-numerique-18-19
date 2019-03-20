2577d0e612b198cbae5b28836a4d59c1c69073fc
Damien Korber <damien@korber.email>
2019-03-20 15:28:36 +0100

    rossie

--- ex6/exercice/ParameterScan.m
+++ ex6/exercice/ParameterScan.m
@@ -32,7 +32,7 @@ for i = 1:nsimul
     % Execution du programme en lui envoyant la valeur a scanner en argument
     cmd = sprintf('%s%s %s %s=%.15g N2=%.15g output=%s', repertoire, executable, input, paramstr, param(i), param(i), output{i});
     disp(cmd);
-    system(cmd);
+    % system(cmd);
 end
 
 %% Analyse %%
@@ -67,15 +67,18 @@ if(strcmp(paramstr,'N1'))
     
     hold on
     plot(N1,err,'k+');
-    poly_approx(log10(N1), log10(err), 1, 2);
-    fit = 10.^fit;
+    [fit, slope] = poly_approx(N1, err, 1, 2, true);
+    plot(fit(:,1), fit(:,2), '-');
+    %fit = 10.^fit;
     set(gca, 'YScale', 'log');
     set(gca, 'XScale', 'log');
+    %poly_approx(dt, Tp, 1, 2, true);
 %     [P,slope]=poly_approx(dt, Tp, 1, 2);
 %     plot(P(1,:),P(2,:));
     xlabel('$N1$')
     ylabel('$|\phi(0)-\phi(0)_th|$ [V]')
-%     legend("slope =" +num2str(slope));
+	legend(["Data", "slope ="+num2str(slope)]);
+    box on;
     grid on
     hold off;
 end
@@ -84,17 +87,42 @@ saveas(f, "graphs/c_convphi","epsc");
 
 %% Fonction
 
-function [polynome, slope] = poly_approx(x, y, ordre, steps)
+function [polynome, slope] = poly_approx(x, y, ordre, steps, isLog)
+    % =================== POLY_APPROX ===================================
+    % RESUMÉ: Permet de faire une approximation d'ordre n d'un set de
+    % données.
+    %
+    % USAGE: Il suffit d'appeller la fonction.
+    %
+    % PARAMETRES:
+    %   - (x/y): Set de donnée de même longueur.
+    %   - order: L'ordre du fit.
+    %          Valeurs acceptables: Tous les entiers plus grand que 0.
+    %   - steps: Nombre de points à sortir. Pour une approximation d'ordre
+    %   1, il est suffisent de prendre 2 points.
+    %          Valeurs acceptables: Tout nombre entier supérieur à 2.
+    %   - isLog: Switch permettant de choisir entre un graphe linéaire et un
+    %   graphe log. 'true' si graphe log, 'false' si graphe linéaire.
+    % ==================================================================
+    if isLog == true
+        x=log10(x);
+        y=log10(y);
+    end
+    
+    % Compute the fit
     pf = polyfit(x, y, ordre);
     slope = pf(1);
     T = linspace(min(x), max(x), steps);
-
     n = ordre + 1;
-
     polynome = zeros(2,length(T));
     for i=1:n
        polynome(2,:) = polynome(2,:) + pf(i)*T.^(n-i);
     end
-    
     polynome(1,:) = T;
+
+    polynome=polynome.'; % transposition pour améliorer l'utilisation.
+    
+    if isLog == true
+       polynome = 10.^polynome; 
+    end
 end
