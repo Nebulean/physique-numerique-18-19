42ac3eba146c6a4abe7b979d3209860215bee9d8
Delphine Martres <dmartres@gmail.com>
2019-04-03 20:34:49 +0200

    mmm

--- ex7/exercice/Analyse.m
+++ ex7/exercice/Analyse.m
@@ -42,7 +42,7 @@ ylabel('f(x,t) [m]')
 ht = title('t=0 s');
 ylim([min(f(:)),max(f(:))])
 for i=2:length(t)
-    pause(.01)
+    pause(.001)
     if ~ishandle(h)
         break % Arrete l'animation si la fenetre est fermee
     end
