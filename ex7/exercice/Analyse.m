5fb8657b7c5f04ad5d2f81a5db43a172ad09b5a5
Delphine Martres <dmartres@gmail.com>
2019-04-07 18:53:09 +0200

    Some graphs for ex1

--- ex7/exercice/Analyse.m
+++ ex7/exercice/Analyse.m
@@ -12,40 +12,57 @@ f = data(:,2:end);
 
 %% Figures %%
 %%%%%%%%%%%%%
-figure('Name',['Analyse de ' fichier])
-subplot(2,2,1)
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+set(groot, 'defaultTextInterpreter', 'latex');
+set(groot, 'defaultAxesFontSize', 18);
+
+% figure('Name',['Analyse de ' fichier])
+% subplot(2,2,1);
+figu=figure;
 plot(x,u)
 grid
-xlabel('x [m]')
-ylabel('u [m/s]')
+xlabel('$x$ [m]')
+ylabel('$u$ [m/s]')
 
-subplot(2,2,2)
+% subplot(2,2,2);
+figE=figure;
 plot(t,E)
 grid
-xlabel('t [s]')
-ylabel('E [m^3]')
+xlabel('$t$ [s]')
+ylabel('$E$ [m$^3$]')
 
-subplot(2,2,4)
+% xt = subplot(2,2,4);
+figxt=figure;
 pcolor(x,t,f)
 shading interp
 colormap jet
 c = colorbar;
-xlabel('x [m]')
-ylabel('t [s]')
+xlabel('$x$ [m]')
+ylabel('$t$ [s]')
 ylabel(c,'f(x,t) [m]')
 
-subplot(2,2,3)
+% fct = subplot(2,2,3);
+figf=figure;
 h = plot(x,f(1,:));
 grid
-xlabel('x [m]')
-ylabel('f(x,t) [m]')
-ht = title('t=0 s');
+xlabel('$x$ [m]')
+ylabel('$f(x,t)$ [m]')
+ht = title('$t=0$ s');
 ylim([min(f(:)),max(f(:))])
 for i=2:length(t)
-    pause(.001)
+    pause(.05)
     if ~ishandle(h)
         break % Arrete l'animation si la fenetre est fermee
     end
     set(h,'YData',f(i,:))
-    set(ht,'String',sprintf('t=%0.2f s',t(i)))
+    set(ht,'String',sprintf('$t=%0.2f$ s',t(i)))
 end
+
+saveas(figE,'graphs/ex1Efixe','epsc');
+saveas(figxt,'graphs/ex1xtfixe','epsc');
+saveas(figf,'graphs/ex1ffixe','epsc');
+
+% saveas(figE,'graphs/ex1Elibre','epsc');
+% saveas(figxt,'graphs/ex1xtlibre','epsc');
+% saveas(figf,'graphs/ex1flibre','epsc');
