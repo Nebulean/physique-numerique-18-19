2a2f356282c0e0ce17ba7f7fd93bc1d1fbdf8d42
Delphine Martres <dmartres@gmail.com>
2019-05-22 15:40:34 +0200

    ii report

--- ex8/exercice/ii.m
+++ ex8/exercice/ii.m
@@ -32,7 +32,13 @@ pmoy = data(:,7);
 % pth=m*(-x0*sin(omega.*t)+sqrt(2*h*omega/m)*cos(omega.*t));
 
 xth=x0*cos(omega.*t)+k0/omega*sin(omega.*t);
-pth=m*(-x0*sin(omega.*t)+k0*cos(omega.*t));
+pth=-x0*m*omega*sin(omega.*t)+k0*cos(omega.*t);
+
+xdiff=abs(xmoy-xth);
+pdiff=abs(pmoy-pth);
+
+[xmax,xid]=findpeaks(xdiff);
+[pmax,pid]=findpeaks(pdiff);
 
 %% Figures
 set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
@@ -40,31 +46,85 @@ set(groot, 'defaultLegendInterpreter', 'latex');
 set(groot, 'defaultTextInterpreter', 'latex');
 set(groot, 'defaultAxesFontSize', 18);
 
-f1=figure;
+% f1=figure;
+% hold on;
+% set(gca, 'fontsize', 25);
+% set(gca, 'LineWidth',1.5);
+% plot(t,xmoy, 'linewidth',1.2);
+% plot(t,xth, 'linewidth',1.2);
+% xlabel('$t ~ [t_P]$');
+% ylabel('$x ~ [\ell_P]$');
+% grid on;
+% box on;
+% legend('$\langle x \rangle (t)$', 'Newtonian $x(t)$');
+% hold off;
+% 
+% f2=figure;
+% hold on;
+% set(gca, 'fontsize', 25);
+% set(gca, 'LineWidth',1.5);
+% plot(t,pmoy,'linewidth',1.2);
+% plot(t,pth,'linewidth',1.2);
+% xlabel('$t ~ [t_P]$');
+% ylabel('$p ~ [m_P ~ c]$');
+% grid on;
+% box on;
+% legend('$\langle p \rangle (t)$', 'Newtonian $p(t)$');
+% hold off;
+% 
+f3=figure;
 hold on;
 set(gca, 'fontsize', 25);
 set(gca, 'LineWidth',1.5);
-plot(t,xmoy, 'linewidth',1.2);
-plot(t,xth, 'linewidth',1.2);
+plot(t,xmoy-xth, 'linewidth',1.2);
 xlabel('$t ~ [t_P]$');
-ylabel('$x ~ [\ell_P]$');
+ylabel('$\langle x \rangle (t)-x(t)~ [\ell_P]$');
 grid on;
 box on;
-legend('$\langle x \rangle (t)$', 'Newtonian $x(t)$');
 hold off;
 
-f2=figure;
+f4=figure;
 hold on;
 set(gca, 'fontsize', 25);
 set(gca, 'LineWidth',1.5);
-plot(t,pmoy,'linewidth',1.2);
-plot(t,pth,'linewidth',1.2);
+plot(t,pmoy-pth,'linewidth',1.2);
 xlabel('$t ~ [t_P]$');
-ylabel('$p ~ [m_P ~ c]$');
+ylabel('$\langle p \rangle (t)-p(t)~ [m_P ~ c]$');
 grid on;
 box on;
-legend('$\langle p \rangle (t)$', 'Newtonian $p(t)$');
 hold off;
 
-saveas(f1,'graphs/ii_x','epsc');
-saveas(f1,'graphs/ii_p','epsc');
+f5=figure;
+hold on;
+set(gca, 'fontsize', 25);
+set(gca, 'LineWidth',1.5);
+plot(t(xid),xmax, 'x', 'markersize',10, 'linewidth',1.2);
+px=polyfit(t(xid),xmax,1);
+plot(t(xid),px(1)*t(xid)+px(2), 'linewidth',1.2);
+xlabel('$t ~ [t_P]$');
+ylabel('$|\langle x \rangle (t)-x(t)| ~ [\ell_P]$');
+grid on;
+box on;
+legend('maxima of $|\langle x \rangle (t)-x(t)|$', "fit: slope="+num2str(px(1)),'location','northwest');
+hold off;
+
+f6=figure;
+hold on;
+set(gca, 'fontsize', 25);
+set(gca, 'LineWidth',1.5);
+plot(t(pid),pmax,'x', 'markersize',10, 'linewidth',1.2);
+pp=polyfit(t(pid),pmax,1);
+plot(t(pid),pp(1)*t(pid)+pp(2), 'linewidth',1.2);
+xlabel('$t ~ [t_P]$');
+ylabel('$|\langle p \rangle (t)-p(t)|~ [m_P ~ c]$');
+grid on;
+box on;
+legend('maxima of $|\langle p \rangle (t)-p(t)|$', "fit: slope="+num2str(pp(1)),'location','northwest');
+hold off;
+
+% saveas(f1,'graphs/ii_x','epsc');
+% saveas(f2,'graphs/ii_p','epsc');
+saveas(f3,'graphs/ii_diffx','epsc');
+saveas(f4,'graphs/ii_diffp','epsc');
+saveas(f5,'graphs/ii_diffxsl','epsc');
+saveas(f6,'graphs/ii_diffpsl','epsc');
